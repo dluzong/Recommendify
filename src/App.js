c5927e651984f7cc52413865d63ca3276cca8ba1
dluzong <daphneluzong@gmail.com>
2024-12-05 06:37:47 -0500

    added express

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import LoginPage from './LoginPage';  
 
 function App() {
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    console.log("Fetching from /api...")
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        console.log("Data fetched: ", data)
+        setBackendData(data);
+      }
+    )
+    .catch(error => console.log("Error fetching data:", error))
+  }, [])
+
   return (
-    <div className="App">
+    <div>
       <LoginPage /> 
     </div>
   );
