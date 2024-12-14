88b02e9d579ba5434ff903a0ca6a1bb92244a887
dluzong <daphneluzong@gmail.com>
2024-12-13 22:38:36 -0500

    tweaked styling + added nav to home

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -1,14 +1,25 @@
 import React from "react";
+import {useNavigate } from 'react-router-dom';
 import "./UserProfile.css";
 
 const UserProfile = () => {
+    const navigate = useNavigate();
+    
+    const handleHome = () => {
+        const storedToken = localStorage.getItem("spotify_token");
+        navigate(`/home?access_token=${storedToken}`);
+    }
 
     return (
         <div className="user-profile">
             <header className="header">
                 <div className="logo">Recommendify</div>
-                <button className="logout-button">Logout</button>
+                <div>
+                    <button className="logout-button" onClick={handleHome}>Home</button>
+                    <button className="logout-button">Logout</button>
+                </div>
             </header>
+
             <div className="profile-info">
                 <div className="profile-picture">
                     <img src="/assets/ProfilePic.avif" alt="Profile" />
@@ -16,6 +27,7 @@ const UserProfile = () => {
                 <h2>User Name</h2> 
                 <div className="line"></div>
             </div>
+
             <div className="profile-data">
                 <div className="left-column">
                     <h3>Playlists</h3>
