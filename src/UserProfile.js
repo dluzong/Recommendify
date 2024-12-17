05932f3205040a2a3a0b755ae2b40100b1f8573a
Jacob Martin <73363952+JakeMartin336@users.noreply.github.com>
2024-12-17 17:00:00 -0500

    Update UserProfile.js
    default-image.jpg

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -92,7 +92,7 @@ const UserProfile = () => {
                                         onClick={() => navigate(`/playlist/${playlist.id}`)}
                                     >
                                         <img 
-                                            src={(playlist.images && playlist.images.length > 0) ? playlist.images[0]?.url : "default-playlist-image.jpg"} 
+                                            src={(playlist.images && playlist.images.length > 0) ? playlist.images[0]?.url : '/assets/default-image.jpg'} 
                                             alt={playlist.name} 
                                             className="playlist-image"
                                         />
